9c47b98b36bfe5d9f75d1cfcebf9f138c96ca769
YandexStudio <jimyandex@outlook.com>
2017-07-14 10:15:43 +0800

    Update Plugin.php

--- Plugin.php
+++ Plugin.php
@@ -89,17 +89,17 @@ class CommentToMail_Plugin implements Typecho_Plugin_Interface
         $port->input->setAttribute('class', 'mini');
         $form->addInput($port->addRule('required', _t('必须填写SMTP服务端口'))
                 ->addRule('isInteger', _t('端口号必须是纯数字')));
-
-        $user = new Typecho_Widget_Helper_Form_Element_Text('user', NULL, NULL,
-                _t('SMTP用户名'),_t('SMTP服务登陆用户名,一般为邮箱名'));
-        $form->addInput($user->addRule('required', _t('SMTP服务验证用户名')));
         
         /*
         适配Sendgrid
         */
-        $frommail = new Typecho_Widget_Helper_Form_Element_Text('user', $user, $user,
-                _t('SMTP发件地址'),_t('SMTP发件地址，一般和用户名相同'));
-        $form->addInput($frommail->addRule('required', _t('SMTP发件地址')));
+        $frommail = new Typecho_Widget_Helper_Form_Element_Text('frommail', NULL, NULL,
+                _t('SMTP用户名'),_t('SMTP服务登陆用户名，一般就是邮箱名'));
+        $form->addInput($frommail->addRule('required', _t('SMTP服务验证用户名')));
+
+        $user = new Typecho_Widget_Helper_Form_Element_Text('user', NULL, $frommail,
+                _t('SMTP发件地址'),_t('SMTP发件地址，一般邮箱请和上方保持一致'));
+        $form->addInput($user->addRule('email', _t('SMTP发件地址，请填写正确的邮件地址！')));
 
         $pass = new Typecho_Widget_Helper_Form_Element_Password('pass', NULL, NULL,
                 _t('SMTP密码'));
