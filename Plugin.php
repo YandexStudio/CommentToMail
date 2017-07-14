eae8e023e1b483e4006aa62047ce34aaac602ac0
YandexStudio <jimyandex@outlook.com>
2017-07-14 10:04:07 +0800

    Update Plugin.php

--- Plugin.php
+++ Plugin.php
@@ -91,15 +91,15 @@ class CommentToMail_Plugin implements Typecho_Plugin_Interface
                 ->addRule('isInteger', _t('端口号必须是纯数字')));
 
         $user = new Typecho_Widget_Helper_Form_Element_Text('user', NULL, NULL,
-                _t('SMTP用户名'),_t('SMTP服务验证用户名,一般为邮箱名'));
+                _t('SMTP用户名'),_t('SMTP服务登陆用户名,一般为邮箱名'));
         $form->addInput($user->addRule('required', _t('SMTP服务验证用户名')));
         
         /*
         适配Sendgrid
-        $frommail = new Typecho_Widget_Helper_Form_Element_Text('user', NULL, NULL,
-                _t('SMTP发件地址'),_t('SMTP发件地址，留空则使用用户名'));
-        $form->addInput($frommail);
         */
+        $frommail = new Typecho_Widget_Helper_Form_Element_Text('user', $user, $user,
+                _t('SMTP发件地址'),_t('SMTP发件地址，一般和用户名相同'));
+        $form->addInput($frommail->addRule('required', _t('SMTP发件地址')));
 
         $pass = new Typecho_Widget_Helper_Form_Element_Password('pass', NULL, NULL,
                 _t('SMTP密码'));
